2efb79cbccd76d8da3a8311938f0ba69e7f37d6c
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-08-25 01:42:59 -0300

    background color

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,9 @@
 export default {
   usePrivate: process.env.USE_PRIVATE || "0", // "1" | "0"
   hsClientSecret: process.env.HIVESIGNER_SECRET || "", // when USE_PRIVATE=0 and HIVESIGNER client section must be provided,
-  hive_id: process.env.HIVE_ID || "",
-  theme: process.env.THEME || "",
-  tags: process.env.TAGS?.split(",") || "",
+  hive_id: process.env.HIVE_ID || "hive-173115",
+  theme: process.env.THEME || "sky",
+  tags: process.env.TAGS?.split(",") || "gnars",
   availibleAccounts: process.env.ACCOUNTS ? +process.env.ACCOUNTS : 0,
   baseApiUrl: process.env.API_URL || "https://account-creator.3speak.tv/api"
 };
